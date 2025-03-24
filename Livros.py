22a7fb33d73c539a3aac865f7eaf005779abe6b3
Rodrigo Loan Cortes Trindade <rodrigoloan@hotmail.com>
2025-03-24 16:49:24 -0300

    adicionando codigos em main(criação de livros) e em livros (futuras funções de conexão com arquivo json)

--- Livros.py
+++ Livros.py
@@ -1,8 +1,18 @@
 class book_overview: # Cria um objeto Livro
     def __init__ (self, id, titulo, ano, genero, autor, disponivel):
-        self.id = int(id)
+        self.id = id
         self.titulo = titulo
-        self.ano = int(ano)
+        self.ano = ano
         self.genero = genero
         self.autor = autor
-        self.disponivel = bool(disponivel)
+        self.disponivel = disponivel
+
+    def exibir_dados(self):
+        return (f"titulo: {self.titulo}, ano: {self.ano}, genero: {self.genero}, autor: {self.autor}")
+    
+    def salvar_json(self):
+        print("Função não construida")
+
+
+    def carregar_json(self):
+        print("Função não construida")
--- Main.py
+++ Main.py
@@ -1,16 +1,43 @@
+from Livros import book_overview 
+from Usuarios import user_type 
+
 def menu():
     while True:
         print("")
-        print("----Main Menu----")
-        print(f"\non going on making a menu to exit press 0 on the keyborad")
+        print("----Menu Principal----")
+        print("")
+        print("1: Cadastro de livros")
+        print("2: Cadastro de Usarios")
+        print("3: ...")
+        print("4: ...")
+        print("0: Sair")
+
 
         option = int(input("Select a option: "))
 
-        if option == 0:
-            print("bye")
+        if option == 1:
+            titulo = input("Digite o Titulo: ")
+
+            while True:
+                ano = input("Qual o ano de lançamento? (somente numeros): ")
+                if ano.isdigit():
+                    break
+                else:
+                    print("Valor inserido não é um numero")
+
+            genero = input("Qual o genero do livro? (Ex: ação, misterio, romance...): ")
+            autor = input("Qual o nome do autor do livro?: ")
+            disponivel = True
+
+            novo_livro = book_overview(id, titulo, ano, genero, autor, disponivel)
+            novo_livro.exibir_dados()
+            novo_livro.salvar_json()
+        elif option == 1:
+            print("função nao codificada")
+
+        elif option == 0:
+            print("Adeus")
             break
-
-
+        else:
+            print("Tente novamente")
 menu()
-
-print("Teste bem sucedido, mas sera mesmo?")
