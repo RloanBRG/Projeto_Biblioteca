65857d0bea18d8750164f49f026646e3abe63709
Rodrigo Loan Cortes Trindade <rodrigoloan@hotmail.com>
2025-05-04 01:52:51 -0300

    Emprestimo e devolução concluido

--- Novo_codigo/view.py
+++ Novo_codigo/view.py
@@ -1,6 +1,6 @@
 from flask import redirect, url_for, render_template, request, session, flash, get_flashed_messages
 from flask_web import app, login_required
-from python.Codigo_json import carregar_json, salvar_json
+from python.Codigo_json import carregar_json, salvar_json, verificar_caminho
 from python.Usuarios import Usuario_Objeto
 import re #importação de autenticação de email ou algo do tipo
 
@@ -79,16 +79,36 @@ def login_home():
 def painel_biblioteca():
     voltar = url_for("inicio")
     sair = url_for('logout')
-
     dados = carregar_json()
-
     if dados is None:
         print("Erro em 'view.py'")
         return "Erro: dados não carregados"        
     
     livros = dados["Livros"]
+    usuario_logado = session["usuario"]
+    livros_emprestados = []
 
-    return render_template("painel_principal.html", livros=livros, voltar=voltar, sair=sair, nome_usuario=session["usuario"]["nome"])
+    for usuario in dados["Usuarios"]:
+        if usuario["nome"] == usuario_logado["nome"] and usuario["email"] == usuario_logado["email"]:
+            emprestado_dict = usuario.get("emprestado", {})
+            break
+        else:
+            emprestado_dict = {}
+
+    for livro in livros:
+        livro_id_str = str(livro["id"])
+        if livro_id_str in emprestado_dict:
+            livros_emprestados.append({
+                "id": livro["id"],
+                "titulo": livro["titulo"],
+                "autor": livro["autor"],
+                "ano": livro["ano"],
+                "genero": livro["genero"],
+                "quantidade_emprestada": emprestado_dict[livro_id_str]
+            })
+
+
+    return render_template("painel_principal.html", livros=livros, livros_emprestados=livros_emprestados, voltar=voltar, sair=sair, nome_usuario=usuario_logado["nome"])
 
 # Sair de um usuario especifico
 @app.route('/logout')
@@ -101,30 +121,31 @@ def logout():
 @login_required
 def emprestar_livros(livro_id):
     if "usuario" not in session:
-        return "Usuario não autenticado"
+        return redirect(url_for("login_home"))
     
     user_secao = session["usuario"]
     dados = carregar_json()
 
-    for livro in dados["Livros"]:
-        if livro["id"] == livro_id and livro["quantidade"] > 0:
-            livro["quantidade"] -= 1
-            if livro["quantidade"] == 0:
-                livro["disponivel"] = False
-            break
-    else:
-        flash("Erro: livro não encontrado","erro")
+    livro = next((l for l in dados["Livros"] if l["id"] == livro_id), None)
+    if not livro or livro["quantidade"] <= 0:
+        flash("Erro: livro não encontrado para emprestimo","erro")
         return redirect(url_for("painel_biblioteca"))
     
+    livro["quantidade"] -= 1
+    if livro["quantidade"] == 0:
+        livro["disponivel"] = False
+
     for usuario in dados["Usuarios"]:
-
-            
-
         if usuario["nome"] == user_secao["nome"] and usuario["email"] == user_secao['email']:
-            if len(usuario["emprestado"]) >= 3:
+            usuario.setdefault("emprestado", {})
+            
+            total_emprestado = sum(usuario["emprestado"].values())
+
+            if total_emprestado >= 3:
                 flash("Erro: limite de emprestimo de livros atingido", "erro")
                 return redirect(url_for("painel_biblioteca"))
-            usuario.setdefault("emprestado", []).append(livro_id)
+            livro_id_str = str(livro_id)
+            usuario["emprestado"][livro_id_str] = usuario["emprestado"].get(livro_id_str, 0) +1
             break
 
     salvar_json(dados)
@@ -135,23 +156,37 @@ def emprestar_livros(livro_id):
 # Devolução de Livros
 @app.route("/devolver_livros/<int:livro_id>", methods=["POST"])
 def devolver_livros(livro_id):
+    if "usuario" not in session:
+        return redirect(url_for("login_home"))
+    
     dados = carregar_json()
+    user_secao = session["usuario"]
+    livro_id_str = str(livro_id)
 
-    usuario = dados["Usuarios"]
+    livro = next((l for l in dados["Livros"] if l["id"] == livro_id), None)
+    if not livro:
+        flash("Erro: livro não encontrado para devolução", "erro")
+        return redirect(url_for("painel_biblioteca"))
+    
+    for usuario in dados["Usuarios"]:
+        if usuario["nome"] == user_secao["nome"] and usuario["email"] == user_secao["email"]:
+            emprestado = usuario.get("emprestado", {})
 
-    if livro_id not in usuario.get("emprestado", []):
-        return render_template("painel_principal.html", livros=dados["Livros"], mensagem="Você não emprestou este livro!")
+            if livro_id_str not in emprestado or emprestado[livro_id_str] <= 0:
+                flash("Erro: você não possui este livro", "erro")
+                return redirect(url_for("painel_biblioteca"))
 
-    usuario['emprestado'].remove(livro_id)
+            emprestado[livro_id_str] -= 1
+            if emprestado[livro_id_str] == 0:
+                del emprestado[livro_id_str]
 
-    for livro in dados['livros']:
-        if livro['id'] == livro_id:
-            if livro['disponivel'] == False:
-                livro['disponivel'] = True
-            livro['quantidade'] += 1
+            livro["quantidade"] += 1
+            if livro["quantidade"] > 0:
+                livro["disponivel"] = True
+
             break
     salvar_json(dados)
-
+    flash("Livro devolvido com sucesso", "sucesso")
     return redirect(url_for("painel_biblioteca"))
 
 # @app.route('/teste_painel')
